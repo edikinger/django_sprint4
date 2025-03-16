80e1bad8b50654059266bac83a9081091f8217ea
Эдуард Агапов <ediking.i45@yandex.ru>
2025-03-17 00:36:50 +0300

    2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -66,9 +66,8 @@ class Post(CreatedAtIsPublished):
     pub_date = models.DateTimeField(
         "Дата и время публикации",
         default=timezone.now,
-        help_text=("Если установить дату и время в будущем"
+        help_text="Если установить дату и время в будущем"
                    " — можно делать отложенные публикации."
-        )
     )
     author = models.ForeignKey(
         User,
